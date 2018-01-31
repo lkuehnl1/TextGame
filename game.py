02eb4692a8ac2cb616c413d5fb2f712f396486b3
lkuehnl1 <35829651+lkuehnl1@users.noreply.github.com>
2018-01-31 17:36:58 -0600

    Added scholar

--- game.py
+++ game.py
@@ -2,8 +2,12 @@ class Background:
     def __init__(self, background, description):
         self.b = background
         self.d = description
-print('which background would you like to learn more about? Soldier?')
-x = Background("Soldier:","You are a millitary trained badass")
+print('which background would you like to learn more about? Soldier? Scholar? Engineer?')
+x = Background("Soldier:","You are a millitary trained badass!")
+y= Background('Engineer:', 'You have spent years studying. Your mind is strong, but your body is weak.')
+
 z=input()
 if z == 'Soldier':
     print(x.b, x.d)
+if z == 'Scholar':
+    print(y.b, y.d)
